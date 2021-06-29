b2574c8fe485d0d0158929588ae8057ff7a8b7d2
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-06-29 19:33:13 -0300

    Projeto Criado

---
+++ app/src/main/java/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.letrasapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
